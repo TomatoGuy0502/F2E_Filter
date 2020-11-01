5ab1814fad3d4872b8cf466cf370a84a52ba6aba
YanTingLin <H34066131@gs.ncku.edu.tw>
2020-11-02 00:22:12 +0800

    feat: Add Vuex

---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state() {
+    return {
+      location: '',
+      time: {
+        start: '',
+        end: ''
+      },
+      categories: {
+        selectAll: true,
+        specific: []
+      }
+    };
+  },
+  mutations: {
+    setLocation(state, location) {
+      state.location = location;
+    },
+    setTime(state, time) {
+      state.time = time;
+    },
+    setCategories(state, categories) {
+      state.categories = categories;
+    }
+  },
+  actions: {
+  }
+});
