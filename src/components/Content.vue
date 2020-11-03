f29392f552154c414794e58ab0c8c6ac76f46082
YanTingLin <H34066131@gs.ncku.edu.tw>
2020-11-03 14:08:55 +0800

    feat: Add content page

---
+++ src/components/Content.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>this is content {{ activeRoute }}</h1>
+</template>
+
+<script>
+import { computed } from "vue";
+import { useRouter } from "vue-router";
+
+export default {
+  name: 'Content',
+  setup() {
+    const router = useRouter()
+    const activeRoute = computed(() => router.currentRoute.value.path)
+    
+    return { activeRoute }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import ActivityList from '../components/ActivityList.vue'
+import Content from '../components/Content.vue'
 import NotFound from '../components/NotFound.vue'
 
 const history = createWebHistory()
 const routes = [
   { path: '/', component: ActivityList },
+  { path: '/content/:_id', component: Content },
   { path: '/:path(.*)*', component: NotFound }
 ]
 
