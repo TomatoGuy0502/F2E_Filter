87dd858812337321b663e8587188dbe22cd2a3cd
YanTingLin <H34066131@gs.ncku.edu.tw>
2020-11-05 16:26:54 +0800

    feat: Change router history mode to Hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import { createWebHashHistory , createRouter } from 'vue-router'
 import ActivityList from '../components/ActivityList.vue'
 import Content from '../components/Content.vue'
 import NotFound from '../components/NotFound.vue'
 
-const history = createWebHistory('/F2E_Filter/')
+const history = createWebHashHistory()
 const routes = [
   { path: '/', component: ActivityList },
   { path: '/content/:_id', name: 'content', component: Content },
